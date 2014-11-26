51f58ee6a5c459ab29e48cdec2e6c804d3b4a2eb
segabito <segabito+gravatar@gmail.com>
2014-11-27 08:25:14 +0900

    コード整形

--- EnqueteOkotowari.user.js
+++ EnqueteOkotowari.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        EnqueteOkotowari
 // @namespace   https://github.com/segabito/
-// @description アンケートおことわり
-// @include     http://www.nicovideo.jp/*
-// @include     http://live.nicovideo.jp/*
+// @description 視聴中のアンケートおことわり
+// @include     http://*.nicovideo.jp/watch/*
+// @include     http://live.nicovideo.jp/watch/*
 // @include     http://seiga.nicovideo.jp/*
 // @version     1
 // @grant       none
@@ -18,14 +18,14 @@
     if (window.enquete) {
       var enquete = window.enquete;
 
+      console.log('%cアンケートおことわりします %s', 'background: lightgreen;', timeoutCounter);
       enquete.openNotification = enquete.insertNotification = enquete.openEnqueteDirectly = enquete.notifyLater;
       enquete.getRemainingMilliSecond = function() { return 0; };
-      enquete.openEnqueteDirectly = enquete.notifyLater
+      enquete.openEnqueteDirectly = enquete.notifyLater;
       enquete.notifyLater();
 
       window.clearInterval(timer);
-    } else 
-    if (timeoutCounter >= 1000) {
+    } else if (timeoutCounter >= 1000) {
       window.clearInterval(timer);
     }
   };
