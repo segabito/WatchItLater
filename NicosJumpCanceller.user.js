1112a1985c828211ac23d257c994b0ba562232df
segabito <segabito+gravatar@gmail.com>
2014-12-13 17:10:55 +0900

    本家の仕様変更で動かなくなっていたのに対応

---
+++ NicosJumpCanceller.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        nicosJumpCanceller
+// @namespace   https://github.com/segabito/
+// @description 「＠ジャンプ」を無効化
+// @include     http://www.nicovideo.jp/watch/*
+// @version     1.0.3
+// @grant none
+// ==/UserScript==
+
+(function() {
+
+  if (!window.require) {
+    return;
+  }
+
+  var monkey = function() {
+    window.WatchJsApi.nicos.addEventListener('nicoSJump', function(e) {
+        e.cancel();
+        require('watchapp/init/PopupMarqueeInitializer').popupMarqueeViewController.onData(
+          '「@ジャンプ」コマンドをキャンセルしました'
+        );
+    });
+  };
+
+  require(['watchapp/model/WatchInfoModel'], function() {
+    var script = document.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('charset', 'UTF-8');
+    script.appendChild(document.createTextNode("(" + monkey + ")()"));
+    document.body.appendChild(script);
+  });
+
+})();
+
