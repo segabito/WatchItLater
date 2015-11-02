2ff3092c380ea46feffe260214f3c59616de36d5
segabito <segabito+gravatar@gmail.com>
2015-11-03 05:44:02 +0900

    OneDriveのフルスクリーン再生の仕様変更に対応

--- OneDriveVideoPlayerFix.user.js
+++ OneDriveVideoPlayerFix.user.js
@@ -1,24 +1,35 @@
 // ==UserScript==
 // @name         OneDrive VideoPlayer fix
 // @namespace    http://github.com/segabito/
-// @version      0.1.4
+// @version      0.3.0
 // @description  OneDriveの動画プレイヤーのフルスクリーン再生を復活&細かい調整
 // @author       segabito
-// @match        https://onedrive.live.com/prev?*
+// @match        https://onedrive.live.com/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
   var monkey = function() {
     var playerFrame = window, video;
+    var ItemVideoPlayerRes;
 
     // https://onedrive.live.com/handlers/clientstring.mvc?mkt=ja-JP&group=Files&v=19.8.1712.2000&useRequiresJs=False
-    var rrZW = top.window.live = top.window.live || {};
-    var uUks = rrZW.shared = rrZW.shared || {};
-    var ccEt = uUks.skydrive = uUks.skydrive || {};
-    var MOFY = ccEt.shared = ccEt.shared || {};
-    var jHBx = MOFY.video = MOFY.video || {};
-    var videoLoadErrorMessage = jHBx["loaderror"] || "問題が発生したため、動画を再生できません。";
+//    var videoLoadErrorMessage = (function() {
+//
+//      var XIEO = window.live = window.live || {};
+//      var smgP = XIEO.shared = XIEO.shared || {};
+//      var idJb = smgP.skydrive = smgP.skydrive || {};
+//      var OLSZ = idJb.monaco = idJb.monaco || {};
+////    qQjl["loaderror"] = "問題が発生したため、動画を再生できません。";
+////    qQjl["loaderrordecode"] = "この動画には問題があります。動画をダウンロードして、ビデオ アプリによる再生を試してください。";
+////    qQjl["loaderrornetwork"] = "動画の再生中に問題が発生しました。インターネット接続を確認し、もう一度やり直してください。";
+// 
+//      var message =
+//          qQjl["loaderror"] ||
+//          jHBx["loaderror"] ||
+//          "問題が発生したため、動画を再生できません。";
+//      return message;
+//    })();
 
 
     var config = (function() {
@@ -44,51 +55,15 @@
       };
     })();
 
-    var __parent_css__ = (function() {/*
-   {* :fullscreen .OneUp-video, *}
-      .fullscreen .OneUp-video
-      {
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-      }
-
-      .OneUp-detailsBar {
-        min-height: inherit !important;
-      }
-
-      .fullscreen .OneUp-detailsBar,
-      .fullscreen .OneUp-commandBar
-      {
-        display: none !important;
-      }
-    */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1].replace(/\{\*/g, '/*').replace(/\*\}/g, '*/');
-
     var __css__ = (function() {/*
-        .fit16-9 .c-VideoPlayer .canvas {
-          bottom: 0 !important;
-        }
 
-        .fit16-9 .c-HtmlDashVideoCanvas,
-        .fit16-9 .c-HtmlDashVideoCanvas     video,
-        .fit16-9 .c-HtmlDashlingVideoCanvas,
-        .fit16-9 .c-HtmlDashlingVideoCanvas video,
-        .fit16-9 .c-HtmlHlsVideoCanvas,
-        .fit16-9 .c-HtmlHlsVideoCanvas      video,
-        .fit16-9 .c-HtmlVideoCanvas,
-        .fit16-9 .c-HtmlVideoCanvas         video
-        {
-          position: absolute;
-          {*background: #666;*}
-          {*top: -8.25%;*}
-          top: -16.666666%;
-          left: 0;
-          width: 100%;
-          height: 133.333333%;
+        .fit16-9 .od-VideoCanvas-video {
+          top: -16.666666% !important;
+          height: 133.333333%  !important;
         }
 
         #ovPanel {
+          display: none;
           position: fixed;
           z-index: 10000;
           top:32px;
@@ -99,6 +74,10 @@
           border: 1px solid #000;
           box-shadow: 4px 4px 0 black;
           opacity: 0;
+        }
+
+        .videoPlayer #ovPanel {
+          display: block;
         }
 
         #ovPanel:hover {
@@ -118,13 +97,21 @@
           color: red;
         }
 
+        {*
         .c-VideoPlayer.isPlaybackPanelVisible .c-PlaybackPanel {
           opacity: 0;
         }
         .c-VideoPlayer.isPlaybackPanelVisible .c-PlaybackPanel:hover {
           opacity: 1;
         }
+        *}
 
+        .fullscreen .OneUp-commandBar, .fullscreen .OneUp-flipper {
+          visibility: visible;
+        }
+        .fullscreen .OneUp-commandBar, .fullscreen .OneUp-flipper:hover {
+          opacity: 0.85;
+        }
     */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1].replace(/\{\*/g, '/*').replace(/\*\}/g, '*/');
 
     var __panel__ = (function() {/*
@@ -194,23 +181,42 @@
     };
 
     var isVideoFinished = function() {
+      var vpos = $('.od-PlaybackPanel--hasDuration .od-PlaybackPanel-timePlayed').text();
+      var duration = $('.od-PlaybackPanel--hasDuration .od-PlaybackPanel-totalTime').text();
+      if (!vpos || !duration) { return false; }
+      //console.log('isVideoFinished', vpos, duration, vpos === duration);
+      return vpos === duration;
       //console.log('"%s","%s"', playerFrame.$('.duration').text(), playerFrame.$('.currentTime').text());
       //console.log(playerFrame.$('.duration').text() === playerFrame.$('.currentTime').text());
-      return playerFrame.$('.duration').text() === playerFrame.$('.currentTime').text();
+      //return playerFrame.$('.duration').text() === playerFrame.$('.currentTime').text();
     };
 
     var isVideoPlayingError = function() {
-      return $('.errorMessage').text() === videoLoadErrorMessage ; // "問題が発生したため、動画を再生できません。";
+      var vp = ItemVideoPlayerRes.strings;
+      var message = $('.od-ItemVideoPlayer-statusText').text();
+      if (!message) { return false; }
+        console.log('statusText', message);
+
+      var isError =
+        (message === vp.errorLoading) ||
+        (message === vp.errorNetwork) ||
+        false;
+
+      if (isError) {
+        console.log('%cVideoPlayingError: %s', 'background: red;', message);
+      }
+      return isError;
+//      return $('.errorMessage').text() === videoLoadErrorMessage ; // "問題が発生したため、動画を再生できません。";
     };
 
     var togglePlay = function() {
-      $('.canvasOverlay').click();
+      $('.od-PlaybackPanel .ms-Icon--play, .od-PlaybackPanel .ms-Icon--pause').click(); //$('.canvasOverlay').click();
     };
 
     var playNext = function() {
       try {
-          var $nextButton = top.window.jQuery('.OneUp-button[data-automationid=nextButton]');
-          top.$nextButton = $nextButton.click();
+          var $nextButton = $('.OneUp-flipper--next .OneUp-button');  //top.window.jQuery('.OneUp-button[data-automationid=nextButton]');
+          window.$nextButton = $nextButton.click();
       } catch (e) {
         console.log('%cException: ', 'background: red;', e, $nextButton);
       }
@@ -227,23 +233,6 @@
       config.set('slideshow', $('body').hasClass('slideshow'));
     };
 
-    var timer = null, count = 0;
-    var onTimer = function() {
-      if (isVideoPlayingError()) {
-        console.log('%cVideo Playing Error. count:%s', 'background: lightgreen;', count++);
-        if (count < 200) {
-          togglePlay();
-          return;
-        }
-      }
-
-      if (config.get('slideshow') && (isVideoFinished() || count >= 200)) {
-        playNext();
-        window.clearInterval(timer);
-        return;
-      }
-    };
-
     var onFullscreenStatusChange = function() {
       var td = top.window.document;
       if (
@@ -258,18 +247,64 @@
       }
     };
 
-    var initialize = function() {
-      console.log('%cinitialize start', 'background: lightgreen;');
-      // for debug
-      parent.window.video = video = $('video')[0];
-      parent.playerFrame = window;
 
-      addStyle(__css__);
-      if (!parent.hasCssAdded) {
-        addStyle(__parent_css__, parent);
-        parent.hasCssAdded = true;
+
+
+
+    var video = null;
+    var isVideoPlayerMode = false;
+    var onVideoWatchTimer = function() {
+      var $video = $('video');
+        if ($video.length < 1) {
+            if (isVideoPlayerMode) {
+                onVideoPlayerClose();
+            }
+            isVideoPlayerMode = false;
+        } else {
+            if (!isVideoPlayerMode) {
+                onVideoPlayerOpen($video);
+            }
+            isVideoPlayerMode = true;
+            _onVideoWatchTimer();
+        }
+    };
+
+    var timer = null, count = 0;
+    var _onVideoWatchTimer = function() {
+      if (isVideoPlayingError()) {
+        console.log('%cVideo Playing Error. count:%s', 'background: lightgreen;', count++);
+        togglePlay();
+        return;
       }
 
+      if (config.get('slideshow') && isVideoFinished()) {
+        console.log('%cVideoFinish', 'background: lightgreen;');
+        playNext();
+        //window.clearInterval(timer);
+        return;
+      }
+    };
+    
+    var onVideoPlayerOpen = function($video) {
+      console.log('%cVideoPlayer Open', 'background: lightgreen;');
+      video = $video[0];
+      // for debug
+      window.video = video;
+      ItemVideoPlayerRes = require('local/controls/video/item/ItemVideoPlayer.resx');
+      $('body').addClass('videoPlayer');
+    };
+    
+    var onVideoPlayerClose = function() {
+      console.log('%cVideoPlayer Close', 'background: lightgreen;');
+      video = null;
+      count = 0;
+      $('body').removeClass('videoPlayer');
+    };
+    
+    var videoWatchTimer = null;
+
+    var initializeEvents = function() {
+      videoWatchTimer = window.setInterval(onVideoWatchTimer, 1000);
       var td = top.window.document;
       td.addEventListener('webkitfullscreenchange', onFullscreenStatusChange);
       td.addEventListener('mozfullscreenchange',    onFullscreenStatusChange);
@@ -281,62 +316,9 @@
         td.removeEventListener('msFullscreenChange',     onFullscreenStatusChange);
         td.removeEventListener('fullscreenchange',       onFullscreenStatusChange);
       };
+    };
 
-      if (!top.ovpWatcherInitialized) {
-        top.ovpWatcherInitialized = true;
-        var watcher = function() {
-            var config = (function() {
-              var prefix = 'OV_';
-              var conf = {
-                slideshow: false,
-                fitMode: '',
-              };
-              return {
-                get: function(key) {
-                  try {
-                    if (window.sessionStorage.hasOwnProperty(prefix + key)) {
-                      return JSON.parse(window.sessionStorage.getItem(prefix + key));
-                    }
-                    return conf[key];
-                  } catch (e) {
-                    return conf[key];
-                  }
-                },
-                set: function(key, value) {
-                  window.sessionStorage.setItem(prefix + key, JSON.stringify(value));
-                }
-              };
-            })();
-            var playNext = function() {
-              try {
-                  var $nextButton = top.window.jQuery('.OneUp-button[data-automationid=nextButton]');
-                  top.$nextButton = $nextButton.click();
-              } catch (e) {
-                console.log('%cException: ', 'background: red;', e, $nextButton);
-              }
-              //window.parent.$('.OneUp-flipper--next').click();
-            };
-            var timer = window.setInterval(function() {
-            	//console.log('timer!',
-            	//	config.get('slideshow'),
-            	//	$('.ThumbnailTile-image:last img').is(':visible'),
-            	//	$('.od-ItemVideoPlayer-frame').length
-            	//	);
-                if (config.get('slideshow') && $('.ThumbnailTile-image:last img').is(':visible') && $('.od-ItemVideoPlayer-frame').length < 1) {
-					//console.log('playNext!');
-                    playNext();
-                }
-            }, 3000);
-        };
-        var script = document.createElement("script");
-        script.id = "OneDriveVideoPlayerFixWatcher";
-        script.setAttribute("type", "text/javascript");
-        script.setAttribute("charset", "UTF-8");
-        script.appendChild(top.document.createTextNode('(' + watcher + ')();'));
-        top.document.body.appendChild(script);
-     }
-
-      parent.$imageButton = $('#ImageButton-20_0').off().on('click', function() { toggleMonitorFull(); });
+    var initializePanel = function() {
       var $panel = $(__panel__);
 
       $panel.find('.fitMode')  .on('click', toggleFitMode);
@@ -347,16 +329,28 @@
 
       $('body').append($panel);
 
-      timer = window.setInterval(onTimer, 1000);
+    };
+    var initialize = function() {
+      console.log('%cinitialize start', 'background: lightgreen;');
+
+      addStyle(__css__);
+
+      initializeEvents();
+        
+      initializePanel();
+
       console.log('%cinitialize end', 'background: lightgreen;');
     };
 
-    var initTimer = window.setInterval(function() {
-      if (window.$) {
-        window.clearInterval(initTimer);
-        window.setTimeout(initialize, 5000);
-      }
-    }, 1000);
+
+      console.log('%cOneDrive VideoPlayer-fix', 'background: lightgreen;');
+
+      var initTimer = window.setInterval(function() {
+        if (window.$) {
+          window.clearInterval(initTimer);
+          window.setTimeout(initialize, 5000);
+        }
+      }, 1000);
   };
 
   var script = document.createElement("script");
@@ -367,27 +361,3 @@
   document.body.appendChild(script);
 })();
 
-/*
- *
-ただのメモ
-playerFrame = ($('.ItemVideoPlayer-frame')[0] && $('.ItemVideoPlayer-frame')[0].contentWindow);
-
-playerFrame.location.href
-playerFrame.$('.errorMessage').text();
-
-
-
-if (playerFrame.$('.errorMessage').text() === "問題が発生したため、動画を再生できません。") { playerFrame.$('.canvasOverlay').click(); }
-
-.c-HtmlDashVideoCanvas, .c-HtmlDashVideoCanvas video, .c-HtmlDashlingVideoCanvas, .c-HtmlDashlingVideoCanvas video, .c-HtmlHlsVideoCanvas, .c-HtmlHlsVideoCanvas video, .c-HtmlVideoCanvas, .c-HtmlVideoCanvas video {
-  position: absolute;
-  background: #666;
-  top: -8.25%;
-  left: 0;
-  width: 100%;
-  height: 116.5%;
-}
-.c-VideoPlayer.isPlaybackPanelVisible .c-PlaybackPanel {
-}
-
-*/
